91728f7fc83e32b3f606466ba65cbcca135c960b
Alexander Emelyanov <emelianoff.casha@mail.ru>
2023-08-04 22:20:05 +0500

    Added image compression

--- blog/users/models.py
+++ blog/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from PIL import Image
 
 
 class User(AbstractUser):
@@ -8,4 +9,14 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+    # method for image compression
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
+        if self.image:
+            img = Image.open(self.image.path)
+
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
