26685b67403e7a0d44ac0e9db5bdb370ae6e134d
Alexander Emelyanov <emelianoff.casha@mail.ru>
2023-08-07 22:46:30 +0500

    Added post update

--- blog/site_blog/views.py
+++ blog/site_blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic import DetailView, CreateView, DeleteView
+from django.views.generic import DetailView, CreateView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 
@@ -46,16 +46,26 @@ class PostDeleteView(LoginRequiredMixin, SuccessMessageMixin, UserPassesTestMixi
         return self.request.user == post.author  # bool
 
 
+class PostUpdateView(LoginRequiredMixin, SuccessMessageMixin, UserPassesTestMixin, TitleMixin, UpdateView):
+    model = Posts
+    fields = ('title', 'content')
+    title = 'Blog - PostUpdate'
+    success_message = 'Запись успешно изменена.'
+    success_url = '/'
+    template_name = 'site_blog/post_create.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
 def about(request):
     context = {
         'title': 'Blog - about'
     }
     return render(request, 'site_blog/about.html', context=context)
 
-# FBV
-# def index(request):
-#     context = {
-#         'title': 'Blog - Main',
-#         'posts': Posts.objects.all(),
-#     }
-#     return render(request, 'site_blog/home.html', context=context)
