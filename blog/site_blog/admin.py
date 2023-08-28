47274c0c2f39e1277986ebda60ddc365440095c2
Alexander Emelyanov <emelianoff.casha@mail.ru>
2023-08-28 20:44:16 +0500

    Added RatingCategoryFilter by PostCategoryAdmin in admin panel

--- blog/site_blog/admin.py
+++ blog/site_blog/admin.py
@@ -5,7 +5,7 @@ from django.db import models
 from .models import Comment, PostCategory, Posts
 
 
-class RatingFilter(admin.SimpleListFilter):
+class RatingPostFilter(admin.SimpleListFilter):
     title = 'Фильтр по рейтингу'
     parameter_name = 'rating'
 
@@ -23,15 +23,36 @@ class RatingFilter(admin.SimpleListFilter):
         return queryset
 
 
+class RatingCategoryFilter(admin.SimpleListFilter):
+    title = 'Фильтр по рейтингу'
+    parameter_name = 'rating'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<3', 'Не популярная'),
+            ('от 3 до 6', 'Средняя'),
+            ('>6', 'Популярная'),
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '<3':
+            return queryset.filter(post_counter__lt=3)
+        elif self.value() == 'от 3 до 6':
+            return queryset.filter(post_counter__gte=3).filter(post_counter__lt=7)
+        elif self.value() == '>6':
+            return queryset.filter(post_counter__gt=6)
+        return queryset
+
+
 @admin.register(Posts)
 class PostsAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'date_posted', 'category', 'rating_post')
-    fields = ('author', 'title', 'content', 'category')
+    fields = ('author', 'category', 'title', 'content',)
     search_fields = ('title__istartswith',)
     ordering = ('-date_posted', '-title')
     list_editable = ('category',)
     list_per_page = 8
-    list_filter = ('category', RatingFilter)
+    list_filter = ('category', RatingPostFilter)
 
     @admin.display(ordering='comments', description='rating')
     def rating_post(self, post: Posts):
@@ -49,12 +70,13 @@ class PostCategoryAdmin(admin.ModelAdmin):
     ordering = ('id', 'name')
     list_editable = ('name',)
     list_per_page = 8
+    list_filter = (RatingCategoryFilter,)
 
     @admin.display(ordering='post_counter', description='Rating')  # calculated field
     def rating_category(self, category: PostCategory):
         if category.post_counter < 3:
             return 'Не популярная'
-        if 3 <= category.post_counter <= 6:
+        elif 3 <= category.post_counter <= 6:
             return 'Средняя'
         return 'Популярная'
 
